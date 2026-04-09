d778276883ecb4e4d2b10f73ab2694822b8445f2
soldernerd <lukas.faessler2@gmail.com>
2026-04-09 09:01:28 +0200

    [v0.6.10] Revert wheel handler back to Canvas (v0.6.3 baseline)

--- LevelApp.Core/AppVersion.cs
+++ LevelApp.Core/AppVersion.cs
@@ -4,7 +4,7 @@ public static class AppVersion
 {
     public const int Major = 0;
     public const int Minor = 6;
-    public const int Patch = 9;
+    public const int Patch = 10;
 
     public static string Full    => $"{Major}.{Minor}.{Patch}";
     public static string Display => $"v{Full}";
