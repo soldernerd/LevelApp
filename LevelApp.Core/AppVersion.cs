5f4ffd2fb4626e5bb50f02e6b1f5f859099ad5b2
soldernerd <lukas.faessler2@gmail.com>
2026-04-09 14:27:03 +0200

    [v0.9.3] Fix dialog crash: use Microsoft.Windows.ApplicationModel.Resources for unpackaged app

--- LevelApp.Core/AppVersion.cs
+++ LevelApp.Core/AppVersion.cs
@@ -4,7 +4,7 @@ public static class AppVersion
 {
     public const int Major = 0;
     public const int Minor = 9;
-    public const int Patch = 2;
+    public const int Patch = 3;
 
     public static string Full    => $"{Major}.{Minor}.{Patch}";
     public static string Display => $"v{Full}";
